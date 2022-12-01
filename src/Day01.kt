a5efa52c94c25e91e0613d04e9d77844853c0eaa
daniyarmukhanov <mukhanov.daniyar+gitlab@gmail.com>
2022-12-01 14:55:39 +0600

    day 1 one-line solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,38 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var max = 0
-        var sum = 0
-        input.forEach {
-            if (it.isNotEmpty()) {
-                sum += it.toInt()
-            } else {
-                if (sum > max) {
-                    max = sum
-                }
-                sum = 0
-            }
-        }
-        return max
-    }
-
-    fun part2(input: List<String>): Int {
-        var sum = 0
-        val list = mutableListOf<Int>()
-        input.forEach {
-            if (it.isNotEmpty()) {
-                sum += it.toInt()
-            } else {
-                list.add(sum)
-                sum = 0
-            }
-        }
-
-        return list.apply { sort() }.takeLast(3).sum()
-
-    }
-
+    fun part1(input: List<String>) = sortBySum(input).last()
+    fun part2(input: List<String>) = sortBySum(input).takeLast(3).sum()
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun sortBySum(input: List<String>): List<Int> {
+    return input.fold(mutableListOf(mutableListOf())) { acc: MutableList<MutableList<String>>, s: String ->
+        if (s.isNotEmpty()) {
+            acc.apply { last().add(s) }
+        } else {
+            acc.apply { add(mutableListOf()) }
+        }
+    }.map { s -> s.sumOf { it.toInt() } }.sorted()
 }
